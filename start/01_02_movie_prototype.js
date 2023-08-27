5ea6704c89a557cdb818f21b4a769564b3e40aae
Ayush Walekar <76579614+AYUSHWAL03@users.noreply.github.com>
2023-08-27 13:29:16 +0000

    2nd challenge is done

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,36 @@
 // Write your code here
+//1st approach
+// class Movie{
+// constructor (title,director,genre,releaseYear,rating){
+//   this.title = title;
+//   this.director = director;
+//   this.genre=genre;
+//   this.releaseYear = releaseYear;
+//   this.rating = rating;
+// }
+
+// movieReview(){
+//   return `${this.title}, a ${this.genre} film directed by ${this.director} was released
+//   in ${this.releaseYear}. It received a rating of ${this.rating} `
+// }
+
+// }
+
+// const movieReviewed = new Movie("title","director","genre","releaseYear",5)
+// console.log(movieReviewed.movieReview());
+
+function Movie(title,director,genre,releaseYear,rating){
+  this.title = title;
+  this.director = director;
+  this.genre=genre;
+  this.releaseYear = releaseYear;
+  this.rating = rating;
+}
+//dont use arrow function here because it have its own scope
+Movie.prototype.movieOverview = function() {
+  return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating} `
+}
+
+const movieReview = new Movie("jailer","Nelson","action",2023,4.6);
+console.log(movieReview.movieOverview())
+
