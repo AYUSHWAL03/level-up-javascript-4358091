65635b0b4cfd63f2fec270b8bf7ff2bdf6f24c3e
Ayush Walekar <76579614+AYUSHWAL03@users.noreply.github.com>
2023-08-28 19:10:42 +0000

    7th challenge done

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,21 @@
 // Write your code here
+function getClassroom(classroom){
+  let {teachingAssistance, classList} = classroom;//destructuring 
+  let teacher,assistant,students;
+
+  if(teachingAssistance){
+    [teacher,assistant,...students] = classList;
+  }
+  else{
+    [teacher,...students] = classList;
+  }
+  return students;
+}
+
+// curly braces for destucturing any json object and square brackets for stucturing an array
+console.log(getClassroom(
+  {
+    teachingAssistance:false,
+    classList:["AYush","Sachin","Sonali","Sai","Ganesh"]
+  }
+))
