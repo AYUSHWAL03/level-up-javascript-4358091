624bf3e127c510891b91ccd40556cb99b38ad94a
Ayush Walekar <76579614+AYUSHWAL03@users.noreply.github.com>
2023-08-28 18:23:14 +0000

    5th challenge done

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,32 @@
 // Write your code here
+//first approach using class
+// class User{
+//   #username
+//   #password
+//   constructor(username,password,age){
+//     this.#username = username;
+//     this.#password = password;
+//     this.age = age;
+//   }
+
+//   getUsername(){
+//     return this.#username;
+//   }
+// }
+
+// const user = new User("Ayush",1234,20);
+// console.log(user.getUsername())
+// console.log(user.username)
+
+//second approach using symbols
+
+const username = Symbol("username");
+const password = Symbol("password");
+
+const user = {
+  [username] : "Ayush",
+  [password] : "12345",
+  age : 20
+}
+
+console.log(user.username);
