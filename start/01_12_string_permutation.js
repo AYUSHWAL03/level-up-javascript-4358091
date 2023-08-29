ab019aa21df32cb83d8ea81aed93e86fca13e6fc
Ayush Walekar <76579614+AYUSHWAL03@users.noreply.github.com>
2023-08-29 19:48:42 +0000

    12th challenge complete

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,11 @@
 // Write your code here
+
+function factorial(num){
+  if(num == 1){
+    return 1;
+  }if(num == 0){
+    return 0;
+  }
+  return factorial(num-1)*(num);
+}
+console.log(factorial(5));
