61b28bab035401b9d2d1c2365dd4e9ff3ee75ee8
Ayush Walekar <76579614+AYUSHWAL03@users.noreply.github.com>
2023-08-29 14:31:44 +0000

    9th challenge done

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,19 @@
 // Write your code here
+//1st approach
+// const numCoffies = [2,1,3,5];
+// const priceCoffee = 1.25;
+// var total = 0;
+// for(var i=0;i<numCoffies.length;i++){
+//   var price = numCoffies[i] * priceCoffee;
+//   total += price;
+// }
+// console.log(` the bill amount is ${total}`);
+
+//2nd approach
+
+function getTotalPrice(coffees,price){
+  const coffeesum = coffees.reduce((total,sum) =>(total += sum))
+  return `The bill amount is ${coffeesum * price}`;
+}
+
+console.log(getTotalPrice([2,1,3,5],5))
