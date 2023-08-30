fc5533a84e7e242b0dac330fdc68a48a69bb963f
Ayush Walekar <76579614+AYUSHWAL03@users.noreply.github.com>
2023-08-30 04:59:40 +0000

    14th challenge done :)

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,8 @@
 // Write your code here
+function urlify(blog){
+  const symbols = /['^",!@&*()$#]/g;
+
+  const blogwithoutsymbols = blog.replace(symbols,"");
+  return blogwithoutsymbols.toLowerCase().trim().replaceAll(" ","-");
+}
+console.log(urlify("I am Ayush!! @AYUSHWAL)3"))
